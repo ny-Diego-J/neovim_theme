fd21c6be665e8b368ad5c48c181fb0f999b67613
GitHub Action <action@github.com>
2025-12-10 14:30:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/nydiegoj/neovimforjetbrains/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nydiegoj/neovimforjetbrains/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.nydiegoj.neovimforjetbrains.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nydiegoj.neovimforjetbrains.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
